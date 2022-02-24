e4d4344b9a44ded4322cd498fa74840860d9f8de
Ayush Nanda <ananda_be18@thapar.edu>
2022-02-24 12:23:12 +0530

    L-1 Authentication & db connection

--- app.js
+++ app.js
@@ -1,1 +1,108 @@
 //jshint esversion:6
+
+// requiring all packages
+const express  = require("express");
+const bodyParser = require("body-parser");
+const ejs  = require("ejs");
+const mongoose = require("mongoose");
+
+const app  = express();
+
+// to use staticfiles
+app.use(express.static("public"));
+//to set ejs
+app.set('view engine', 'ejs');
+//to use bodyparser
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+//connecting mongodb database
+mongoose.connect("mongodb://localhost:27017/userDB", {UseNewUrlParser: true});
+
+// creating schema
+const userSchema = {
+
+    email : String,
+    password: String
+
+};
+
+//creating model from schema
+const User = new mongoose.model("User",userSchema);
+
+
+//get methods
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+
+//Making register functionality working and save user and show secrets page
+
+app.post("/register", function(req,res){
+
+    const newUser = new User({
+
+        email: req.body.username,
+        password: req.body.password
+
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Registered User Logged in");
+            res.render("secrets");
+        }
+    }); 
+
+
+});
+
+ 
+// making login functionality working
+
+app.post("/login",function(req,res){
+
+    //that  user entered
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //verifying with db
+    User.findOne({email: username},function(err,foundUser){
+        if(err){
+            console.log(err);
+        } else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+
+    });
+
+
+
+});
+
+
+
+
+//to listen to port
+app.listen(3000,function(req,res){
+
+    console.log("Server started on port 3000");
+
+});
+
