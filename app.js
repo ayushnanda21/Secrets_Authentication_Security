536281fb116ff3c403e5fb1baef9bf83930aa93c
Ayush Nanda <ananda_be18@thapar.edu>
2022-02-28 17:13:19 +0530

    L-4 Salting and Hashing, bcrypt(17k/sec)

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const express  = require("express");
 const bodyParser = require("body-parser");
 const ejs  = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+//const md5 = require("md5");
 //const encrypt = require('mongoose-encryption');
 const app  = express();
 
@@ -58,23 +61,29 @@ app.get("/register",function(req,res){
 
 app.post("/register", function(req,res){
 
-    const newUser = new User({
+    bcrypt.hash(req.body.password, saltRounds ,function(err,hash){
 
-        email: req.body.username,
-        password: md5(req.body.password)
+        const newUser = new User({
+
+            email: req.body.username,
+            password: hash
+    
+        });
+    
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                console.log("Registered User Logged in");
+                res.render("secrets");
+            }
+        }); 
+    
+    
 
     });
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            console.log("Registered User Logged in");
-            res.render("secrets");
-        }
-    }); 
-
-
+    
 });
 
  
@@ -84,7 +93,7 @@ app.post("/login",function(req,res){
 
     //that  user entered
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = (req.body.password);
 
     //verifying with db
     User.findOne({email: username},function(err,foundUser){
@@ -92,9 +101,13 @@ app.post("/login",function(req,res){
             console.log(err);
         } else{
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password,foundUser.password,function(err,result){
+
+                        if(result === true){
+                            res.render("secrets");
+                        }
+
+                });
             }
         }
 
